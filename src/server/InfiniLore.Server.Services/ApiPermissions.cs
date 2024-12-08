af7b405e986b0d992d1a7ab253741ee3a1f7af77
Anna Sas <anna.sas.dev@gmail.com>
2024-12-08 16:11:56 +0100

    Feat: ApiPermissions

---
+++ src/server/InfiniLore.Server.Services/ApiPermissions.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using AterraEngine.DependencyInjection;
+using InfiniLore.Permissions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InfiniLore.Server.Services;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[InjectableService<ApiPermissions>(ServiceLifetime.Singleton)]
+[PermissionsStore(GeneratorFlags.ParsePrefix)]
+public partial class ApiPermissions {
+    // -----------------------------------------------------------------------------------------------------------------
+    // SectionNames
+    // -----------------------------------------------------------------------------------------------------------------
+    private const string Data = nameof(Data);
+    private const string User = nameof(User);
+    private const string System = nameof(System);
+    private const string Discovery = nameof(Discovery);
+    
+    private const string Account = nameof(Account);
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Permissions
+    // -----------------------------------------------------------------------------------------------------------------
+    [Prefix(Data, User)] public partial string LorescopeRead { get; } // can read data from user's lorescopes
+    [Prefix(Data, User)] public partial string LorescopeWrite { get; } // can write data to user's lorescopes
+    [Prefix(Data, User)] public partial string LorescopeDelete { get; } // can delete  user's lorescopes
+    [Prefix(Data, User)] public partial string LorescopeManage { get; } // can manage  user's lorescopes
+    [Prefix(Data, Discovery)] public partial string LorescopeDiscover { get; } // Can look up lorescopes
+    [Prefix(Data, System)] public partial string LorescopesInfo { get; } // can read system info about lorescopes
+}
